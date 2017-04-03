8e155f25e6063e67e07d6fb4ba52a775c4a54752
Eric Atempa <ea242@njit.edu>
2017-04-03 12:35:55 -0400

    added product list/delete

--- tech_support/product_manager/index.php
+++ tech_support/product_manager/index.php
@@ -1,12 +1,12 @@
 <?php
 require('../model/database.php');
-// require('../model/product_db.php');
+require('../model/products.php');
 
 $action = filter_input(INPUT_POST, 'action');
 if ($action === NULL) {
     $action = filter_input(INPUT_GET, 'action');
     if ($action === NULL) {
-        $action = 'under_construction';
+        $action = 'list_products';
     }
 }
 
@@ -14,5 +14,19 @@ switch ($action) {
     case 'under_construction':
         include('../under_construction.php');
         break;
+    case 'list_products':
+        $products = get_products();
+        include('product_list.php');
+    break;
+    case 'delete_product':
+        $product_id = filter_input(INPUT_POST, 'product_id');
+        if ($product_id == NULL || $product_id == FALSE) {
+            $error = "Missing or incorrect product id or category id.";
+            include('../errors/error.php');
+        } else { 
+            delete_product($product_id);
+            header("Location: .?");
+        }
+    break;
 }
 ?>
